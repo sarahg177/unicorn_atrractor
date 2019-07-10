4c172813646bd63dc9f974f917c8bf1bb715327f
Sarah <sgriggs177@yahoo.co.uk>
2019-07-10 06:46:34 +0100

    Features added to ticket list

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,4 +4,5 @@ from tickets import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('ticket_list', views.get_ticket_list, name='ticket_list'),
+    path('single_view', views.view_ticket_details, name='single_view')
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from tickets.models import Ticket
+from .forms import CommentForm
 
 
 def welcome(request):
@@ -19,3 +20,19 @@ def get_ticket_list(request):
     )
 
     return render(request, "view_tickets_list.html", {'bugs': bugs, 'features': features})
+
+
+def view_ticket_details(request, pk):
+    """Create a view that will return a single ticket object based on the ticket ID(pk)
+    and render it to the 'view_single_ticket.html' template showing comments made. Or return a 404 error if the post is
+    not found"""
+    ticket = get_object_or_404(request, pk=pk)
+    ticket.views += 1
+    ticket.save()
+    form = CommentForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('single_view')
+    else:
+        form = CommentForm()
+    return render(request, "view_single_ticket.html", {'bug': ticket}, {'form': form})
