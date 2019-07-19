4a18067908ea0e09155bb1682d93962a54a664f8
Sarah <sgriggs177@yahoo.co.uk>
2019-07-19 19:53:45 +0100

    Revert "products app added"
    This reverts commit 318dab60f7062992f34eea1b62d6d13c6111c3c7.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	products/views.py
    
    take out secret key

--- products/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Product
-
-class ProductTests(TestCase):
-    """Test against Product models"""
-    def test_str(self):
-        test_name = Product(name='A Product')
-        self.assertEqual(str(test_name), 'A Product')
--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'tickets',
-    'products'
 ]
 
 MIDDLEWARE = [
