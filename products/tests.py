cfe1802b5623cee2cec40a8d8cccda11a3691f7a
Sarah <sgriggs177@yahoo.co.uk>
2019-07-21 20:06:17 +0100

    Feature payment added using stripe

--- products/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Product
-
-
-class ProductTests(TestCase):
-    """Tests again Product model"""
-    def test_str(self):
-        test_name = Product(name='A Product')
-        self.assertEqual(str(test_name), 'A Product')
--- unicorn_atrractor/urls.py
+++ unicorn_atrractor/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('tickets.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
 ]
