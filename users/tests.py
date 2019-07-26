2afd52ea631208a7820e38aecc2d40c34d9d615d
Sarah <sgriggs177@yahoo.co.uk>
2019-07-26 09:25:54 +0100

    users tests written

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import User
 
-# Create your tests here.
+
+class TestUser(TestCase):
+    def setUp(self):
+        """Runs at the start of the test"""
+        self.client = Client()
+
+    def test_create_user(self):
+        """Tests the user model"""
+        User.objects.create(username='Joe', password='bloggs')
+        assert len(User.objects.all()) == 1
+
+    def test_register_view(self):
+        """Test the register page renders correctly"""
+        reg = self.client.get('/users/register/')
+        assert b"<input" in reg.content
+        assert b"username" in reg.content
+        assert b"password1" in reg.content
+        assert b"password2" in reg.content
+
+    def test_registration_was_succesful(self):
+        """Test that the user has registered successfully and then redirected"""
+        reg = self.client.post('/users/register/', {'username': 'joe',
+                                                    'password1': 'bloggsbloggs',
+                                                    'password2': 'bloggsbloggs'})
+        assert reg.status_code == 302
