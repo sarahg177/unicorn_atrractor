26ae36b0b5e544febde0c060df52deab7c2a672c
Sarah <sgriggs177@yahoo.co.uk>
2019-07-28 19:56:22 +0100

    import env added to settings.py

--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 #import dj_database_url
 
-if os.path.exists('env.py')
+if os.path.exists('env.py'):
     import env
 
 if os.environ.get('DEVELOPMENT'):
