7f89eeb31a8fd88a49d430358298d764e159759d
Sarah <sgriggs177@yahoo.co.uk>
2019-07-30 06:30:49 +0100

    prep requirements and settings file for deployment

--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-#import dj_database_url
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -88,15 +88,15 @@ WSGI_APPLICATION = 'unicorn_atrractor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#if development:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-#else:
-   # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+# if development:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# else:
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
