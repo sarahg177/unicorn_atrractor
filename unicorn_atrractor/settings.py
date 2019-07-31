b40ef84e5ce98e68a9d1ecf1f78b9a5731f2cc82
Sarah <sgriggs177@yahoo.co.uk>
2019-07-31 05:16:37 +0100

    added Heroku and Postgres support for deployment

--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'unicorn_atrractor.urls'
@@ -138,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
