c1da94c1330ecbf1f55df4e46cdbe719d28aa4a3
Sarah <sgriggs177@yahoo.co.uk>
2019-08-26 11:51:29 +0100

    settings.py updated to host files on aws s3

--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'users',
     'tickets',
     'blog',
-    'graphs'
+    'graphs',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -140,9 +141,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+
+AWS_STORAGE_BUCKET_NAME = 'sarah-unicorn-attractor'
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
