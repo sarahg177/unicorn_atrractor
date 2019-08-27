d2cff84f664109fb27bb13b61b698a876b02a556
Sarah <sgriggs177@yahoo.co.uk>
2019-08-27 06:32:53 +0100

    images changed on carousel

--- unicorn_atrractor/settings.py
+++ unicorn_atrractor/settings.py
@@ -91,19 +91,18 @@ WSGI_APPLICATION = 'unicorn_atrractor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if development:
-    print("Postgres URL not found, using sqlite instead")
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Postgres URL not found, using SQLite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
